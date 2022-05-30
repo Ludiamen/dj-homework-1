bfa05fa9195f6f35bf90e2111072c2f3af2ae805
Ludiamen <maxvas1001@gmail.com>
2022-05-30 08:01:08 +0300

    added the required tests

--- 3.4-django-testing/django_testing/django_testing/settings.py
+++ 3.4-django-testing/django_testing/django_testing/settings.py
@@ -81,6 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_django_testing',
+        'USER': 'netology',
+        'PASSWORD': 'welersevatel',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -124,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+}
